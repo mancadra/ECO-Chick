bfbf32f3328e9585dee4de819975a2824793f678
Jure <jure.mrzek15@gmail.com>
2023-04-18 11:52:00 +0100

    Added different screens

--- core/src/com/mygdx/game/ChickenGame.java
+++ core/src/com/mygdx/game/ChickenGame.java
@@ -1,31 +1,29 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.Screens.*;
 
-public class ChickenGame extends ApplicationAdapter {
+public class ChickenGame extends Game {
 	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(this, batch));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 50, 50);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		System.exit(0);
 	}
 }
---
+++ core/src/com/mygdx/game/Screens/PlayScreen.java
@@ -0,0 +1,58 @@
+package com.mygdx.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.ChickenGame;
+
+public class PlayScreen implements Screen {
+    ChickenGame game;
+    SpriteBatch batch;
+    Texture img;
+    public PlayScreen(ChickenGame game, SpriteBatch batch){
+        this.game = game;
+        this.batch = batch;
+    }
+
+    @Override
+    public void show() {
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, 50, 50);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+    }
+}
